b2708acf4274198fc73c96c8001930e9fb503920
Seb Jansen <Seb@Normiebook.local>
2016-06-20 20:47:25 +0200

    Refreshed POJO and added dependencies for Retrofit

--- ThrowMyLife/app/src/main/java/com/example/seb/throwmylife/models/PlainScore.java
+++ ThrowMyLife/app/src/main/java/com/example/seb/throwmylife/models/PlainScore.java
@@ -1,44 +1,59 @@
+
 package com.example.seb.throwmylife.models;
 
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class PlainScore {
 
     @SerializedName("playerName")
+    @Expose
     private String playerName;
-
     @SerializedName("score")
+    @Expose
     private int score;
-
     @SerializedName("date")
+    @Expose
     private int date;
 
-    public PlainScore(String playerName, int score, int date) {
-        this.playerName = playerName;
-        this.score = score;
-        this.date = date;
-    }
-
+    /**
+     * @return The playerName
+     */
     public String getPlayerName() {
         return playerName;
     }
 
+    /**
+     * @param playerName The playerName
+     */
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
 
+    /**
+     * @return The score
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * @param score The score
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * @return The date
+     */
     public int getDate() {
         return date;
     }
 
+    /**
+     * @param date The date
+     */
     public void setDate(int date) {
         this.date = date;
     }
@@ -51,4 +66,5 @@ public class PlainScore {
                 ", date=" + date +
                 '}';
     }
+
 }
